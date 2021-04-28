533fcf11218b5bdf2a1e07f81c081f17fdfbce35
G2 <sirgawain@g2entgroup.com>
2021-04-28 01:38:29 -0400

    Added brand image and Hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { Box, Text, Flex, Heading } from "@chakra-ui/react";
 import { Container } from '../components/common/container';
 import CreativeCard from '../components/CreativeCard';
+import BrandHero from '../components/BrandHero';
+
 export default function Home() {
   return (
     <Box flex="1" height="100vh">
@@ -9,6 +11,7 @@ export default function Home() {
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <BrandHero />
       <Container>
         <Heading as="h2" size="lg" my={4}>Active Brand Campaigns</Heading>
         <CreativeCard />
