0f849a56465b46fd2f62b2657f1efabb9ee2621e
G2 <sirgawain@g2entgroup.com>
2021-05-01 19:04:06 -0400

    made updates so that Fleek will compile
    Made changes to support Fleek hosting and IPFS storage.
    
    Signed-off-by: G2 <sirgawain@g2entgroup.com>

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    trailingSlash: true,
+    exportPathMap: function() {
+      return {
+        '/': { page: '/' }
+      };
+    },
+    future: {
+      webpack5: true,
+    },
+    webpack: function (config, options) {
+      config.experiments = {};
+      return config;
+    },
+};
