f3e63a0b40eb45e996e19219bf93d5fca021a89a
G2 <sirgawain@g2entgroup.com>
2021-05-01 19:17:19 -0400

    update
    Signed-off-by: G2 <sirgawain@g2entgroup.com>

--- src/components/SignupModal.tsx
+++ src/components/SignupModal.tsx
@@ -1,15 +1,1 @@
-import React, { ReactElement } from 'react'
-
-interface Props {
-    
-}
-
-function SignupModal({}: Props): ReactElement {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default SignupModal
+export {}
