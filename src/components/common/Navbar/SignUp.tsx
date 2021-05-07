c698c1696af7d1cc997668d555fcc9fa7360b100
ChaoChaoChao <chao.li@usask.ca>
2021-05-08 01:30:04 +0800

    upload the signup modal
    about 70%  finish, will add input element later.

--- src/components/common/Navbar/SignUp.tsx
+++ src/components/common/Navbar/SignUp.tsx
@@ -9,6 +9,8 @@ import {
   ModalCloseButton,
   useDisclosure,
   Button,
+  Flex,
+  Box,
 } from "@chakra-ui/react";
 import Image from "next/image";
 
@@ -21,11 +23,24 @@ const SignUp = () => {
         Sign Up
       </Button>
 
-      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+      <Modal isOpen={isOpen} onClose={onClose} size="xl" isCentered>
         <ModalOverlay />
-        <ModalContent>
-          <ModalBody>
-           
+        <ModalContent borderRadius="10px" h="400px">
+          <ModalCloseButton />
+          <ModalBody
+            borderRadius="10px"
+            bgGradient="linear(to-r, rgb(40, 92, 163), rgb(229, 1, 105))"
+            h="400px"
+          >
+            <Flex alignItems="center" pt="15%" justifyContent="space-between">
+              <Image
+                src="https://www.creativeplatform.io/img/Creative_logo.png"
+                alt="Creative Logo"
+                width={200}
+                height={200}
+              />
+              <Box >Register / Sign Up</Box>
+            </Flex>
           </ModalBody>
         </ModalContent>
       </Modal>
